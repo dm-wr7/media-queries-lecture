b9334de3a6c9e96538ffefeab8b62939a518f03e
Andrew Westenskow <andrew.westenskow@gmail.com>
2020-12-04 14:35:44 -0500

    Finished lecture

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
 
 function App() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  function toggleMenu() {
+    setIsMenuOpen(!isMenuOpen)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <h1>THE ANDY ZONE</h1>
+        <nav className="nav-bar">
+          <p className="nav-item">Home</p>
+          <p className="nav-item">About</p>
+          <p className="nav-item">Contact</p>
+        </nav>
+        <img
+          onClick={toggleMenu}
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/1024px-Hamburger_icon.svg.png"
+          className="hamburger"
+        />
+        <nav className={`mobile-nav ${isMenuOpen ? null : 'mobile-nav-hide'}`}>
+          <p className="mobile-nav-item">Home</p>
+          <p className="mobile-nav-item">About</p>
+          <p className="mobile-nav-item">Contact</p>
+        </nav>
       </header>
+
+      <div className={`red-box ${isMenuOpen ? 'red-box-animate' : null}`}></div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
